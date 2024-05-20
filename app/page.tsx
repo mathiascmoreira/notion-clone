5fda394d57bda5351ec38ceda04fe6eec6ecd840
mathiascmoreira <mathiascmoreira@gmail.com>
2024-05-20 00:40:06 -0600

    environment setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="purple" >
+        Click me
+      </Button>
+    </div>
+  );
+}
+ 
